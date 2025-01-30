425b9104d6f014767d137becc100b46f0803d42f
Julius Adekanbi Asaolu <adevikthur@gmail.com>
2025-01-30 02:32:26 -0500

    js added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let pointBoard = document.getElementById("point-board");
+
+let point = 0;
+
+pointBoard.textContent = point;
+
+function addPoints1() {
+    pointBoard.textContent += 1;
+}
+
+addPoints();
